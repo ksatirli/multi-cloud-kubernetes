6bf2cc0c709f007d1eb7b70f983faeadabe76617
Kerim Satirli <k.satirli@gmail.com>
2021-10-10 22:06:07 +0200

    linting

--- outputs/remote_state.tf
+++ outputs/remote_state.tf
@@ -16,16 +16,3 @@ data "terraform_remote_state" "clusters" {
     }
   }
 }
-
-# This Output iterates over all Workspaces in `var.tfe_workspaces`
-# and creates a List of Object containing all-Workspace Outputs.
-# see https://www.terraform.io/docs/language/values/outputs.html
-output "clusters" {
-  description = "Assembled List of Objects containing per-Workspace Outputs"
-
-  value = {
-    # see https://www.terraform.io/docs/language/meta-arguments/for_each.html
-    for cluster in var.tfe_workspaces :
-    cluster => data.terraform_remote_state.clusters[cluster].outputs
-  }
-}
