373bb0ce13f1dc00fe6b69e45be5736b8077eecf
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-10-11 16:05:50 -0400

    Update Vault configuration

--- vault/variables.tf
+++ vault/variables.tf
@@ -1,11 +1,17 @@
 variable "do_region" {
   type        = string
-  description = "he slug identifier for the region where the resources will be created."
+  description = "The slug identifier for the region where the resources will be created."
   default     = "sfo3" # San Francisco, CA
 }
 
 variable "do_token" {
   type        = string
   description = "This is the DO API token."
+  sensitive   = true
+}
+
+variable "vault_token" {
+  type        = string
+  description = "Vault root token for dev mode."
   sensitive   = true
 }
