45d7922fc4be3a45329f0fbd78e234838cb77607
Kerim Satirli <k.satirli@gmail.com>
2021-10-11 16:22:10 +0200

    moving files

--- vault/providers.tf
+++ vault/providers.tf
@@ -6,6 +6,9 @@ provider "digitalocean" {
 
 # see https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
 provider "kubernetes" {
+  # see https://www.terraform.io/docs/language/providers/configuration.html#alias-multiple-provider-configurations
+  alias = "doks"
+
   cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
   host                   = data.digitalocean_kubernetes_cluster.cluster.endpoint
   token                  = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
@@ -13,6 +16,9 @@ provider "kubernetes" {
 
 # see https://registry.terraform.io/providers/hashicorp/helm/latest/docs
 provider "helm" {
+  # see https://www.terraform.io/docs/language/providers/configuration.html#alias-multiple-provider-configurations
+  alias = "doks"
+
   kubernetes {
     cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
     host                   = data.digitalocean_kubernetes_cluster.cluster.endpoint
