22b2fd98b9a3f4b1cc1c21456327f5e4771fd049
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-10-12 09:45:30 +0100

    adding transit key and encrypt/decrypt policies

--- vault/modules/vault/main.tf
+++ vault/modules/vault/main.tf
@@ -14,3 +14,44 @@ resource "helm_release" "vault" {
     })
   ]
 }
+
+# See https://registry.terraform.io/modules/devops-rob/transit-secrets-engine/vault/latest
+
+module "transit_secrets_engine" {
+  source  = "devops-rob/transit-secrets-engine/vault"
+  version = "0.1.0"
+
+  transit_keys = [
+    {
+      name                   = "payments_service"
+      allow_plaintext_backup = false
+      convergent_encryption  = false
+      exportable             = false
+      deletion_allowed       = true
+      derived                = false
+      type                   = "rsa-2048"
+      min_decryption_version = 1
+      min_encryption_version = 1
+    }
+  ]
+}
+
+resource "vault_policy" "payments_service_encryptor" {
+  name = "payments_service_encryptor"
+
+  policy = <<EOF
+path "transit/encrypt/payments_service" {
+   capabilities = [ "update" ]
+}
+EOF
+}
+
+resource "vault_policy" "payments_service_decryptor" {
+  name = "payments_service_decryptor"
+
+  policy = <<EOF
+path "transit/decrypt/payments_service" {
+   capabilities = [ "update" ]
+}
+EOF
+}
