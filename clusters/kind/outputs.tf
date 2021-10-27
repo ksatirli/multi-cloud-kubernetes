61db0f5340a8184fb9bf681ad2f1d18744a1060f
Kerim Satirli <ksatirli@users.noreply.github.com>
2021-10-27 13:26:08 +0200

    Update clusters/kind/outputs.tf

--- clusters/kind/outputs.tf
+++ clusters/kind/outputs.tf
@@ -32,5 +32,5 @@ output "cluster_endpoint" {
 # this variable is used for testing purposes and has no bearing on the demo
 # see https://www.terraform.io/docs/language/values/outputs.html
 output "workspace_url" {
-  value = "https://app.terraform.io/app/kind-local-organization/workspaces/${var.tfe_workspaces_prefix}-kind"
+  value = "https://app.terraform.io/app/a-demo-organization/workspaces/${var.tfe_workspaces_prefix}-kind"
 }
