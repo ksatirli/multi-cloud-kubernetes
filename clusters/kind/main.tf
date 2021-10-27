429c89d22197bca3776587b4ef05850f1d518539
Kerim Satirli <ksatirli@users.noreply.github.com>
2021-10-27 11:49:44 +0200

    Update main.tf

--- clusters/kind/main.tf
+++ clusters/kind/main.tf
@@ -11,15 +11,19 @@ resource "kind_cluster" "cluster" {
   kind_config {
     kind        = "Cluster"
     api_version = "kind.x-k8s.io/v1alpha4"
+    
     node {
       role = "control-plane"
+      
       kubeadm_config_patches = [
         "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
       ]
+      
       extra_port_mappings {
         container_port = 80
         host_port      = 80
       }
+      
       extra_port_mappings {
         container_port = 443
         host_port      = 443
