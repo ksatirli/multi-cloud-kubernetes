a2bb6d919cec772174f166f7226026da76097045
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-03-06 19:36:50 +0100

    chore: rm gke

--- gke/service_accounts.tf
+++
@@ -1,6 +0,0 @@
-# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
-resource "google_service_account" "cluster" {
-  account_id   = "cluster"
-  display_name = "Cluster Service Account (${var.tfe_workspaces_prefix}-gke)"
-  project      = var.google_project
-}
--- gke/setup/README.md
+++
@@ -1,3 +0,0 @@
-# GCP base Setup
-
-This directory contains Terraform Resources to create a GCP Service Account for Terraform Cloud.
