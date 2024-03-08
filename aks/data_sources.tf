1477235c4249e8abc9340f06a10a216ac72e6b89
Kerim Satirli <k.satirli@gmail.com>
2024-03-08 11:11:18 +0100

    updates cluster version to 1.29 and uses preview mode

--- aks/data_sources.tf
+++ aks/data_sources.tf
@@ -1,6 +1,8 @@
 # see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/kubernetes_service_versions
 data "azurerm_kubernetes_service_versions" "cluster" {
   location        = var.azure_region
-  version_prefix  = "1.19"
-  include_preview = false
+  version_prefix  = "1.29"
+
+  # at time of publishing, `1.29` was only available in preview
+  include_preview = true
 }
