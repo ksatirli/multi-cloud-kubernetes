abd18130e51668c3b3deb5ca4e37b724902a1401
Kerim Satirli <k.satirli@gmail.com>
2024-03-08 11:11:38 +0100

    updates AZ AKS module to new format

--- aks/main.tf
+++ aks/main.tf
@@ -9,14 +9,20 @@ module "aks" {
   source  = "Azure/aks/azurerm"
   version = "8.0.0"
 
-  resource_group_name             = azurerm_resource_group.cluster.name
-  agents_count                    = 3
-  enable_http_application_routing = true
-  kubernetes_version              = data.azurerm_kubernetes_service_versions.cluster.latest_version
-  orchestrator_version            = data.azurerm_kubernetes_service_versions.cluster.latest_version
-  os_disk_size_gb                 = 100
-  prefix                          = var.tfe_workspaces_prefix
-  vnet_subnet_id                  = module.network.vnet_subnets[0]
+  prefix              = var.tfe_workspaces_prefix
+  resource_group_name = azurerm_resource_group.cluster.name
+  kubernetes_version  = data.azurerm_kubernetes_service_versions.cluster.latest_version
+
+  admin_username         = null
+  azure_policy_enabled   = true
+
+  #checkov:skip=CKV_AZURE_4:Logging is disabled for demo purpose. Re-enable for production environments.
+  log_analytics_workspace_enabled   = false
+  net_profile_pod_cidr              = "10.1.0.0/16"
+  private_cluster_enabled           = true
+  rbac_aad                          = true
+  rbac_aad_managed                  = true
+  role_based_access_control_enabled = true
 
   # see https://developer.hashicorp.com/terraform/language/meta-arguments/depends_on
   depends_on = [
